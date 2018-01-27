a70050ac02c4eded7bf09dfcea235ce9b80af00f
Nazar Maksymchuk <nzrbeats@gmail.com>
2018-01-27 07:51:16 -0800

    Vue

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# mantenn.github.io
-<h1>Portfolio</h1>
+# portfolio
+
+> projects and a little bio/interests
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/Project.vue
@@ -0,0 +1,40 @@
+<template>
+    <span class="project" v-bind:class="{hidden: isHidden}">
+        <h3>
+            <a href="{{project.path}}">
+                {{project.title}}
+            </a>
+        </h3>
+		<p>
+            {{project.description}}
+        </p>
+    </span>
+</template>
+
+<script>
+    export default {
+        name: "Project",
+        data () {
+            return {
+                projects: {
+                    [{
+
+                    }],
+                    [{
+                        
+                    }],
+                    [{
+
+                    }],
+                    [{
+
+                    }],
+                    [{
+                        
+                    }]
+                }
+            }
+        }
+
+    }
+</script>
